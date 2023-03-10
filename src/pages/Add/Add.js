fb003dda79a0e8f4bb1d855e3c94c1b3006f2ff2
okubo <sho6210@outlook.jp>
2023-03-10 16:09:48 +0900

    fix: ラジオボタンの挙動バグを修正

--- src/pages/Add/Add.js
+++ src/pages/Add/Add.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useToggle } from "react-use";
 import { v4 as uuidv4 } from "uuid";
 import Contents from "containers/Contents";
+import "./style.scss";
 
 const Add = (props) => {
+  const answers = ["True", "False"];
+  const [answer, setAnswer] = useState("True");
+  const [questionBool, setQuestionBool] = useToggle(false);
+
   // 問題集
   const [newBook, setNewBook] = useState({
     id: uuidv4(),
@@ -14,20 +21,31 @@ const Add = (props) => {
 
   // 問題
   const [question, setQuestion] = useState({
-    correct: true,
     id: uuidv4(),
+    answer: answer,
     desc: "",
   });
 
+  // 正答を設定
+  const swichAnswer = (value) => {
+    setAnswer(value);
+    setQuestion((state) => ({
+      ...state,
+      answer: value,
+    }));
+  };
+
+  // 問題集を作成
   const addBook = () => {
     if (newBook.questions.length !== 0) {
       if (newBook.title === "") {
         setNewBook((state) => ({ ...state, title: "無題" }));
       }
-      props.setBooks((state) => [...state, newBook]);
 
+      // props.setBooks((state) => [...state, newBook]);
+
+      // 初期化
       setNewBook({
-        //初期化
         id: uuidv4(),
         title: "",
         count: 0,
@@ -38,8 +56,10 @@ const Add = (props) => {
     }
   };
 
+  // 問題を作成
   const addQuestion = () => {
     if (question.desc !== "") {
+      setQuestionBool(true);
       setNewBook((state) => ({
         ...state,
         questions: [...state.questions, question],
@@ -48,65 +68,108 @@ const Add = (props) => {
       alert("問題文を入力してください");
     }
 
+    // 初期化
     setQuestion({
-      //初期化
       id: uuidv4(),
-      correct: true,
+      answer: answer,
       desc: "",
     });
   };
 
   return (
     <Contents>
-      <div>
-        <input
-          type="text"
-          value={newBook.title}
-          onChange={(e) => {
-            setNewBook((state) => ({ ...state, title: e.target.value }));
-          }}
-        />
-        <div>
-          <div>
+      <div className="add">
+        <div className="heading">
+          <h1>新規ワークブック</h1>
+        </div>
+        <div className="body">
+          <div className="item">
+            <h2>タイトル</h2>
             <input
+              className="textbox"
               type="text"
-              value={question.desc}
+              value={newBook.title}
               onChange={(e) => {
-                setQuestion((state) => ({ ...state, desc: e.target.value }));
+                setNewBook((state) => ({ ...state, title: e.target.value }));
               }}
             />
-            <div>
-              <input
-                type="radio"
-                name="correct"
-                onChange={(e) => {
-                  setQuestion((state) => ({ ...state, correct: true }));
-                }}
-              />
-              ○
-              <input
-                type="radio"
-                name="correct"
-                onChange={(e) => {
-                  setQuestion((state) => ({ ...state, correct: false }));
-                }}
-              />
-              ✕
-            </div>
           </div>
 
-          <button onClick={addQuestion}>問題を追加</button>
+          <div className="item">
+            <h2>問題作成</h2>
+            <div className="form">
+              <div className="form__item">
+                <p>
+                  問題文<span>100文字まで入力できます。</span>
+                </p>
+                <textarea
+                  maxlength="100"
+                  className="textbox"
+                  type="text"
+                  value={question.desc}
+                  onChange={(e) => {
+                    setQuestion((state) => ({
+                      ...state,
+                      desc: e.target.value,
+                    }));
+                  }}
+                />
+              </div>
+
+              <div className="form__item">
+                <p>
+                  正答<span>クイズの正答を選んでください。</span>
+                </p>
+                <div className="container">
+                  {answers.map((value) => {
+                    return (
+                      <label htmlFor="answer" key={value}>
+                        <input
+                          className="radio"
+                          type="radio"
+                          name="answer"
+                          checked={value === answer}
+                          onChange={(e) => {
+                            swichAnswer(value);
+                          }}
+                        />
+                        <span>{value}</span>
+                      </label>
+                    );
+                  })}
+                </div>
+              </div>
+
+              <div className="form__item">
+                <div>
+                  <button onClick={addQuestion}>問題を追加</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div style={{ visibility: questionBool ? "visible" : "hidden" }}>
+              <div className="list">
+                <ul>
+                  {newBook.questions.map((element, i) => {
+                    return (
+                      <li key={element.id}>
+                        <p className="num">{i + 1}</p>
+                        <p className="content">{element.desc}</p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      {/* 一覧表示 */}
-      <ul>
-        {newBook.questions.map((element) => {
-          return <li key={element.id}>{element.desc}</li>;
-        })}
-      </ul>
       <div>
-        <button onClick={addBook}>決定</button>
+        <button onClick={addBook}>問題集を作成</button>
+        <Link to={"/"}>
+          <button>キャンセル</button>
+        </Link>
       </div>
     </Contents>
   );
