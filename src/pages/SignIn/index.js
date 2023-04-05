5655ebcb6a02fde3516b5095fac06103bdb161cf
okubo <sho6210@outlook.jp>
2023-04-05 19:12:15 +0900

    update: デザイン変更

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import Button from "../../components/Button";
 
 import { auth, provider } from "../../firebase";
+import tb1 from "../../images/tb1.png";
+import tb2 from "../../images/tb2.png";
 
 import "./style.scss";
 
 const SignIn = () => {
-
   const SignInWithGoogle = () => {
     auth.signInWithPopup(provider);
   };
@@ -14,26 +15,30 @@ const SignIn = () => {
   return (
     <>
       <section className="signIn">
-        <div className="background"></div>
         <div className="body">
-          <div className="heading">
-            <h1>
-              暇つぶしにも、
-              <br />
-              勉強にも。
-            </h1>
-            <h2>○×クイズビルダー</h2>
-            <p>誰でも簡単に○×クイズを作成し、遊ぶことのできるアプリです。</p>
-          </div>
-          <div className="formArea">
-            <p>サインイン</p>
-            <div className="menuArea">
+          <div className="fv">
+            <div className="heading">
+              <h1>QuizPAL</h1>
+              <h2>○×クイズビルダー</h2>
+              <p>誰でも簡単に○×クイズを作成し、遊ぶことのできるアプリです。</p>
+            </div>
+            <div className="formArea">
               <Button
                 sizes="normal"
                 styles="filledButton"
                 child={"Googleでログインする"}
                 onClick={SignInWithGoogle}
               />
+            </div>
+          </div>
+          <div className="container">
+            <div className="images">
+              <div className="imageWrapper">
+                <img src={tb2} alt="" />
+              </div>
+              <div className="imageWrapper">
+                <img src={tb1} alt="" />
+              </div>
             </div>
           </div>
         </div>
